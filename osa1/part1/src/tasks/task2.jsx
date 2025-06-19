b277db4ab609f7a7489ea62281be391b370e9691
susnezhu <susan.klimo@gmail.com>
2025-06-19 15:09:13 +0300

    exercise 1.6

--- osa1/part1/src/tasks/task2.jsx
+++ osa1/part1/src/tasks/task2.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
 const Task2 = () => {
-    return (
-        <p>task 2 i'll put here</p>
-    )
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback!</h1>
+
+      <Button onClick={() => setGood(good + 1)} text="Good"/>
+      <Button onClick={() => setNeutral(neutral +1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad" />
+
+      <h2>statistics:</h2>
+
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+
+    </div>
+  );
 }
 
 export default Task2;
