8dfe197048c9624de852adc889fd150a98e1fdd8
susnezhu <susan.klimo@gmail.com>
2025-06-25 17:01:02 +0300

    exercise 2.9

--- osa2/part2/src/tasks/task2.jsx
+++ osa2/part2/src/tasks/task2.jsx
@@ -3,15 +3,17 @@ import {useState} from 'react'
 const Task2 = () => {
 
     const allPersons = [
-        {name: "Arto Hellas",
-        number: "040-12345678"
-        }
-    ]
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]
 
     const [persons, setPersons] = useState(allPersons)
 
     const [newName, setNewName] = useState("")
     const [newNumber, setNewNumber] = useState("")
+    const [newFilter ,setNewFilter] = useState("")
 
     const nameFieldChange = (event) => {
         setNewName(event.target.value)
@@ -20,10 +22,14 @@ const Task2 = () => {
         setNewNumber(event.target.value)
     }
 
-    const nameExists = persons.some(person => person.name === newName)
+    const filterFieldChange = (event) => {
+        setNewFilter(event.target.value)
+    }
 
     const addPerson = (event) => {
         event.preventDefault()
+
+        const nameExists = persons.some(person => person.name === newName)
         
         if (!nameExists) {
             const personObject = {
@@ -44,6 +50,9 @@ const Task2 = () => {
         <div>
             <h1>Phonebook</h1>
 
+            <div>filter shown with <input value={newFilter} onChange={filterFieldChange}/></div>
+
+            <h2>Add a new</h2>
             <form>
                 <div>name: <input value={newName} onChange={nameFieldChange}/></div>
                 <div>number: <input value={newNumber} onChange={numberFieldChange}/></div>
@@ -57,11 +66,14 @@ const Task2 = () => {
             <h2>Numbers:</h2>
 
             <ul>
-                {persons.map(num =>
-                    <li key={num.name}>
-                        {num.name}: {num.number}
+                {persons.filter(person => 
+                person.name.toLowerCase().includes(newFilter.toLowerCase()) ||
+                person.number.includes(newFilter))
+                .map(filteredPerson => (
+                    <li key={filteredPerson.name}>
+                        {filteredPerson.name}: {filteredPerson.number}
                     </li>
-                )}
+                ))}
             </ul>
         </div>
     )
