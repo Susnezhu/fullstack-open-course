acb19730e92c1c48aec76d5aeb0ffdfe5556b2e0
susnezhu <susan.klimo@gmail.com>
2025-06-25 13:18:15 +0300

    exercise 2.8

--- osa2/part2/src/tasks/task2.jsx
+++ osa2/part2/src/tasks/task2.jsx
@@ -3,15 +3,21 @@ import {useState} from 'react'
 const Task2 = () => {
 
     const allPersons = [
-        {name: "Arto Hellas"}
+        {name: "Arto Hellas",
+        number: "040-12345678"
+        }
     ]
 
     const [persons, setPersons] = useState(allPersons)
 
     const [newName, setNewName] = useState("")
+    const [newNumber, setNewNumber] = useState("")
 
-    const fieldChange = (event) => {
+    const nameFieldChange = (event) => {
         setNewName(event.target.value)
+    }
+    const numberFieldChange = (event) => {
+        setNewNumber(event.target.value)
     }
 
     const nameExists = persons.some(person => person.name === newName)
@@ -21,13 +27,16 @@ const Task2 = () => {
         
         if (!nameExists) {
             const personObject = {
-                name: newName}
+                name: newName,
+                number: newNumber}
 
             setPersons(persons.concat(personObject))
             setNewName("")
+            setNewNumber("")
         } else {
             alert(`${newName} is already added to phonebook`)
             setNewName("")
+            setNewNumber("")
         }
     }
 
@@ -36,20 +45,21 @@ const Task2 = () => {
             <h1>Phonebook</h1>
 
             <form>
-                <div>
-                    name: <input value={newName} onChange={fieldChange}/>
-                </div>
+                <div>name: <input value={newName} onChange={nameFieldChange}/></div>
+                <div>number: <input value={newNumber} onChange={numberFieldChange}/></div>
+
                 <div>
                     <button onClick={addPerson} type="submit">add</button>
                 </div>
             </form>
+
 
             <h2>Numbers:</h2>
 
             <ul>
                 {persons.map(num =>
                     <li key={num.name}>
-                        {num.name}
+                        {num.name}: {num.number}
                     </li>
                 )}
             </ul>
