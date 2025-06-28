fc54a4c77bfae92fd5f1209035f3883dee58b965
susnezhu <susan.klimo@gmail.com>
2025-06-28 10:05:29 +0300

    exercise 2.18

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,5 +1,6 @@
 import Task1 from "./tasks/task1/task1";
 import Task2 from "./tasks/task2/task2";
+import Task3 from "./tasks/task3/task3";
 
 const App = () => {
   return (
@@ -9,6 +10,10 @@ const App = () => {
       <hr />
 
       <Task2 />
+
+      <hr />
+
+      <Task3 />
 
     </div>
   )
---
+++ osa2/part2/src/tasks/task3/task3.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const Task3 = () => {
+    const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all"
+
+    const [countries, setCountries] = useState([])
+    const [newFilter, setNewFilter] = useState("")
+
+    const hook = () => {
+        axios.
+        get(baseUrl)
+        .then(response => {
+            const countriesData = response.data
+            const countryObject = countriesData.map(country => ({
+                name: country.name.common,
+                capital: country.capital,
+                languages: country.languages ? Object.values(country.languages) : [],
+                area: country.area,
+                flag: country.flags.png,
+                alt: country.flags.alt
+            }))
+
+            setCountries(countryObject)
+        })
+    }
+    useEffect(hook, [])
+    
+
+    const handleInputChange = (event) => {
+        setNewFilter(event.target.value)
+    }
+
+    const filteredCountries = countries.filter(country =>
+        country.name.toLowerCase().includes(newFilter.toLowerCase())
+    )
+
+
+    return (
+        <div>
+            <h1>Search countries</h1>
+
+            <div>
+                find countries <input value={newFilter} onChange={handleInputChange} />
+            </div>
+
+            <CountryList countries={filteredCountries} />
+        </div>
+    )
+}
+
+const CountryList = ({countries}) => {
+    if (countries.length > 10) {
+        return <p>Too many matches, specife another filter</p>
+    } else if (countries.length === 1) {
+        return <CountryDetail country={countries[0]} />
+    } else {
+        return (
+            <ul>
+                {countries.map(country => (
+                    <li key={country.name}>{country.name}</li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+const CountryDetail = ({country}) => {
+    return (
+        <div>
+            <h2>{country.name}</h2>
+
+            <p>Capital: {country.capital}</p>
+            <p>Area: {country.area}</p>
+
+            <p>Languages:</p>
+            <ul>
+                {country.languages.map(lang => (
+                    <li key={lang}>{lang}</li>
+                ))}
+            </ul>
+
+            <img src={country.flag} alt={country.alt} width={150} />
+        </div>
+    )
+}
+
+export default Task3
