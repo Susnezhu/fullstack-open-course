6464ad1c05a65763bea734c826709ba4b884bad1
susnezhu <susan.klimo@gmail.com>
2025-07-11 14:19:05 +0300

    node_modules deleted and exercise 3.12

---
+++ osa3/Mongo-pages/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+    console.log("give pasword as argument")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = "phonebook"
+
+const url = `mongodb+srv://phonebook:${password}@phonebook.pu0h1t0.mongodb.net/${name}?retryWrites=true&w=majority&appName=Phonebook`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+
+    const note = new Person({
+        name: newName,
+        number: newNumber
+    })
+
+    note.save().then(result => {
+        console.log(`added ${newName} number ${newNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
+
+
--- osa3/REAMDME.md
+++
@@ -1,7 +0,0 @@
-# Linkki puhelinluettelo sivuille:
-https://fullstack-open-course-dau8.onrender.com/
-
-
-
-# Tuli myös ongelma, kun tein tehtävää. Tästä sain ratkaisun:
-https://stackoverflow.com/questions/79553495/throw-new-typeerrormissing-parameter-name-at-i-debug-url
