5556421dbf8f1d61e0afa73b8df34bc200e27bc0
susnezhu <susan.klimo@gmail.com>
2026-05-08 15:25:08 +0300

    part 8 - chapter 3

--- osa8/fs-graphql/library-frontend/src/components/Authors.jsx
+++ osa8/fs-graphql/library-frontend/src/components/Authors.jsx
@@ -1,8 +1,40 @@
+import { useQuery } from '@apollo/client/react'
+import { useMutation } from '@apollo/client/react'
+import { useState } from 'react'
+
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../../queries'
+
 const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+
+  const [selected, setSelected] = useState('')
+  const [birthYear, setBirthYear] = useState('')
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [
+      { query: ALL_AUTHORS }
+    ],
+  })
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (authors.loading) {
+    return <p>loading...</p>
+  }
+
+  const submitChanges = (event) => {
+    event.preventDefault()
+
+    if (!selected) {
+      return null
+    }
+
+    editAuthor({ variables: { name: selected, setBornTo: Number(birthYear) } })
+
+    console.log('changes submitted', selected)
+  }
 
   return (
     <div>
@@ -14,15 +46,52 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {authors.data.allAuthors.map((a) => (
             <tr key={a.id}>
-              <td>{a.name}</td>
+              <td><button onClick={() => setSelected(a.name)}>select</button>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div> {/* Maybe later I will add "hide and show" button*/}
+        <h3>Change author</h3>
+
+        <label>
+          Selected author:
+          <select 
+            name="selectedAuthor" 
+            value={selected}
+            onChange={e => setSelected(e.target.value)}
+          >
+          <option disabled value="">
+            Select author
+          </option>
+
+          {authors.data.allAuthors.map((a) => (
+              <option value={a.name}>{a.name}</option>
+          ))}
+          </select>
+        </label>
+
+
+        
+
+        <form onSubmit={submitChanges}>
+          <div>
+            born
+            <input
+              type="number"
+              value={birthYear}
+              onChange={({ target }) => setBirthYear(target.value)}
+            />
+          </div>
+          
+          <button type="submit">Change author</button>
+        </form>
+      </div>
     </div>
   )
 }
--- osa8/fs-graphql/library-frontend/src/components/Books.jsx
+++ osa8/fs-graphql/library-frontend/src/components/Books.jsx
@@ -1,9 +1,16 @@
+import { useQuery } from '@apollo/client/react'
+import { ALL_BOOKS } from '../../queries'
+
 const Books = (props) => {
+  const books = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (books.loading) {
+    return <p>loading...</p>
+  }
 
   return (
     <div>
@@ -16,7 +23,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.data.allBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author}</td>
