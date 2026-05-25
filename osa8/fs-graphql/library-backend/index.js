9bbdfb91891cc30e54e83ed31d756bef1faca527
susnezhu <susan.klimo@gmail.com>
2026-05-25 13:25:53 +0300

    Getting mongodb uri changed

--- osa8/fs-graphql/library-backend/index.js
+++ osa8/fs-graphql/library-backend/index.js
@@ -4,9 +4,8 @@ const connectToDatabase = require('./db')
 const startServer = require('./server')
 
 const MONGODB_URI =
-  process.env.NODE_ENV === 'test'
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI
+  process.env.TEST_MONGODB_URI ||
+  process.env.MONGODB_URI
 
 const PORT = process.env.PORT || 4000
 
