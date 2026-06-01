4b9888d14e97a220e8bed71e5c9dfe0410130656
susnezhu <susan.klimo@gmail.com>
2026-06-01 16:36:30 +0300

    fixing eslint errors

--- osa9/fs-typescript/healthapp/index.ts
+++ osa9/fs-typescript/healthapp/index.ts
@@ -3,28 +3,37 @@ import { calculateBmi  } from './bmiCalculator.ts';
 import { calculateExercises } from './exerciseCalculator.ts';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   return res.send('Hello Full Stack!');
 });
 
 app.get('/bmi', (req, res) => {
+  type Results = {
+    weight: number,
+    height: number,
+    bmi: string
+  };
+
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
 
   if (!height || !weight) {
-    return res.status(400).send({error: "malformatted parameters"})
+    return res.status(400).send({error: "malformatted parameters"});
   };
 
-  let result = {}
+  const result : Results = {
+    weight: 0,
+    height: 0,
+    bmi: ''
+  };
 
   try {
-    result = {
-      weight: weight,
-      height: height,
-      bmi: calculateBmi(height, weight)
-    };
+    result.weight = weight;
+    result.height = height;
+    result.bmi = calculateBmi(height, weight);
+
   } catch (error) {
     return res.status(400).send({
       error: (error as Error).message
@@ -35,25 +44,42 @@ app.get('/bmi', (req, res) => {
 });
 
 app.post('/exercises', (req, res) => {
-  const exercises = req.body.daily_exercises
-  const target = req.body.target
+
+  type Result = {
+    periodLength: number
+    trainingDays: number
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+  };
+
+  type ExerciseRequest = {
+    daily_exercises: number[];
+    target: number;
+  };
+  const body = req.body as ExerciseRequest;
+
+  const exercises = body.daily_exercises;
+  const target = body.target;
 
   if (exercises == null || target == null) {
     return res.status(400).send({error: "parameters missing"});
   }
 
-  let result = {}
+  let result : Result;
 
   try {
     result = calculateExercises(exercises, target);
   } catch (error) {
     return res.status(400).send({
       error: (error as Error).message
-    })
+    });
   }
 
-  return res.send(result)
-})
+  return res.send(result);
+});
 
 const PORT = 3000;
 
